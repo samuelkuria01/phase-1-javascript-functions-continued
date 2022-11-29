1871d3da367eb4fb393569de803c7e2c76d7b53e
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2022-11-30 00:11:22 +0300

    a round of applause for trying

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(fun){
+console.log(`This Saturday, i want to ${fun}`);
+}
+
+saturdayFun("bathe my dog");
+ saturdayFun("roller-skate");
+ 
+
+ 
+function mondayWork(work){
+    return (`This Monday, I will ${work}`);
+}
+mondayWork('I will work from home.');
+mondayWork('go to the office');
+
+
+function wrapAdjective(adj="a hard worker"){
+     console.log(`You are *${adj}*!`);
+
+    }
+    wrapAdjective();
+
+    function wrapAdjective(string=" a dedicated programmer"){
+         console.log(`You are ||${string}||!`);
+    }
+        wrapAdjective();
